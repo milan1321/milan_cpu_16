84f9b97fe1f5e7604456ec2b1d436cb1e098104b
milan <milan@scaledge.io>
2023-02-16 12:40:07 +0530

    commiting sub design

--- sub.v
+++ sub.v
@@ -1,4 +1,4 @@
-module fs_nbit(D, borr, A, B, Ci);
+/*module fs_nbit(D, borr, A, B, Ci);
   parameter N=16;
   input [N-1:0] A, B;
   input Ci;
@@ -12,5 +12,66 @@ module fs_nbit(D, borr, A, B, Ci);
     {borr, D} = A + (~B) + (~Ci);
   
   end
-endmodule;
+endmodule;*/
+// Code your design here
+module 16bitsub(A,B,Ci,D,borr);
+  parameter N=16;
+  input [N-1:0] A, B;
+  input Ci;
+  output reg [N-1:0] D;
+  output reg borr;
+  wire [N:0]b;
+  
+  genvar i;
+  generate
+    assign b[0]=bin;
+    for(i=0; i<=N-1; i++)begin
+      fs f0(.A(A[i]),.B(B[i]),.Ci(Ci[i]),.D(D[i]),.borr(b[i+1]));
+      
+    end
+    assign borr=b[N];
+  endgenerate
+endmodule
+
+module fs(output reg D, borr, input A, B, Ci);
+  wire b,b1,sub1;
+  hs h1(.A(A),.B(B),.borr(b),.D(sub1));
+  hs h2(.A(A),.B(Ci),.borr(b1),.D(sub));
+  or h3(borr,b1,b);
+endmodule
+
+module hs(output reg D, borr, input A, B);
+  xor g1(D, A, B);
+  and g2(borr, A, B);
+endmodule// Code your design here
+module 16bitsub(A,B,Ci,D,borr);
+  parameter N=16;
+  input [N-1:0] A, B;
+  input Ci;
+  output reg [N-1:0] D;
+  output reg borr;
+  wire [N:0]b;
+  
+  genvar i;
+  generate
+    assign b[0]=bin;
+    for(i=0; i<=N-1; i++)begin
+      fs f0(.A(A[i]),.B(B[i]),.Ci(Ci[i]),.D(D[i]),.borr(b[i+1]));
+      
+    end
+    assign borr=b[N];
+  endgenerate
+endmodule
+
+module fs(output reg D, borr, input A, B, Ci);
+  wire b,b1,sub1;
+  hs h1(.A(A),.B(B),.borr(b),.D(sub1));
+  hs h2(.A(A),.B(Ci),.borr(b1),.D(sub));
+  or h3(borr,b1,b);
+endmodule
+
+module hs(output reg D, borr, input A, B);
+  xor g1(D, A, B);
+  and g2(borr, A, B);
+endmodule
 
